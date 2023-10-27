fe7fb3a2568cac175e48fec2745a856bf92b8824
학원 <test@test.com>
2023-10-27 19:46:45 +0900

    초기환경세팅

---
+++ src/main/java/com/memo/test/TestController.java
@@ -0,0 +1,48 @@
+package com.memo.test;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.memo.post.mapper.PostMapper;
+
+@Controller
+public class TestController {
+	
+	@Autowired
+	private PostMapper postMapper;
+	
+	
+	@ResponseBody
+	@GetMapping("/test1")
+	public String test1() {
+		return "hello world";
+	}
+	
+	@ResponseBody
+	@GetMapping("/test2")
+	public Map<String, Object> test2() {
+		Map<String, Object> map = new HashMap<>();
+		map.put("a", 1);
+		map.put("b", 200);
+		return map;
+	}
+	
+	@GetMapping("/test3")
+	public String test3() {
+		return "test/test";
+	}
+	
+	@ResponseBody
+	@GetMapping("/test4")
+	public List<Map<String, Object>> test4() { // 이렇게 쓰면 안됨  =>   무조건 Domain으로
+		return postMapper.selectPostList();
+	}
+	
+	
+}
